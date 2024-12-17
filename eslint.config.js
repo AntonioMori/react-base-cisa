b4c21e19c66ffe28a808fa2fdf91695ccb6ba7ad
Antonio Mori <camargomoritrancoso@hotmail.com>
2024-12-16 21:50:24 -0300

    added stylistic/eslint-plugin

--- eslint.config.js
+++ eslint.config.js
@@ -3,37 +3,39 @@ import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 import reactRefresh from "eslint-plugin-react-refresh";
+import stylistic from "@stylistic/eslint-plugin";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {
-    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
-    languageOptions: {
-      globals: globals.browser,
-    },
-  },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
-  {
-    plugins: {
-      "react-refresh": reactRefresh,
-      "@stylistic": require("@stylistic/eslint-plugin"), // Adiciona o plugin Stylistic
-    },
-    rules: {
-      // React Refresh
-      "react-refresh/only-export-components": [
-        "warn",
-        { allowConstantExport: true },
-      ],
+	{
+		files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+		languageOptions: {
+			globals: globals.browser,
+		},
+	},
+	pluginJs.configs.recommended,
+	...tseslint.configs.recommended,
+	pluginReact.configs.flat.recommended,
+	{
+		plugins: {
+			"react-refresh": reactRefresh,
+			"@stylistic": stylistic, // Importando o plugin corretamente no ESM
+		},
+		rules: {
+			// React Refresh
+			"react-refresh/only-export-components": [
+				"warn",
+				{ allowConstantExport: true },
+			],
+			"react/react-in-jsx-scope": "off", // Desativa a exigência de React no escopo
 
-      // Stylistic Rules
-      "@stylistic/indent": ["error", "tab", { SwitchCase: 1 }], // Usa tabulação para indentação ao invés de spaces
-      "@stylistic/ts/semi": ["error", "always"], // Sempre usa ponto e vírgula no final
-      "@stylistic/quotes": ["error", "double"], // Usa aspas duplas
-      "@stylistic/bracket-spacing": ["error", "always"], // Adiciona espaços dentro de chaves { chave: valor }
-      "@stylistic/brace-style": ["error", "1tbs", { allowSingleLine: false }], // Coloca chaves em linhas diferentes
-      "@stylistic/arrow-parens": ["error", "always"], // Sempre adiciona parênteses em arrow functions
-    },
-  },
+			// Stylistic Rules
+			"@stylistic/indent": ["error", "tab", { SwitchCase: 1 }], // Usa tabulação para indentação ao invés de spaces
+			"@stylistic/semi": ["error", "always"], // Sempre usa ponto e vírgula no final
+			"@stylistic/quotes": ["error", "double"], // Usa aspas duplas
+			"@stylistic/object-curly-spacing": ["error", "always"], // Adiciona espaços dentro de chaves { chave: valor }
+			"@stylistic/brace-style": ["error", "1tbs", { allowSingleLine: false }], // Coloca chaves em linhas diferentes
+			"@stylistic/arrow-parens": ["error", "always"], // Sempre adiciona parênteses em arrow functions
+		},
+	},
 ];
